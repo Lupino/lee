371d3bb5443bbe0063dee348152f7bf1d0ce157b
Ubasic <lmjubuntu@gmail.com>
2013-12-10 19:43:55 +0800

    update Model

--- lee/models.py
+++ lee/models.py
@@ -2,27 +2,43 @@ from .utils import unparse
 
 __all__ = ['Model']
 
-class Model(dict):
+class Model(object):
 
     table_name = 'default'
     columns = []
     auto_cache = True
     cache_timeout = 0
     auto_create_table = True
-    __slots__ = ['_table']
+    __slots__ = ['_table', '_payload']
 
     def __init__(self, table, payload = {}):
         if payload:
             payload = unparse(payload, self.columns)
-        dict.__init__(self, payload)
+
         self._table = table
+        self._payload = payload
 
     def __repr__(self):
         columns = '\n#. '.join([str(col) for col in self.columns])
         return 'Table[{}] columns: \n#. {}'.format(self.table_name, columns)
 
+    def __getitem__(self, key, default=None):
+        '''x.__getitem__(y) <==> x[y]'''
+        return self._payload.get(key, default)
+
+    def __setitem__(self, key, val):
+        '''x.__setitem__(i, y) <==> x[i]=y'''
+        if isinstance(val, str):
+            val = val.strip()
+        self._payload[key] = val
+
+    __getattr__ = __getitem__
+
+    def copy(self):
+        return self._payload.copy()
+
     def save(self):
-        return self._table.save(self.copy())
+        return self._table.save(self._payload.copy())
 
     def delete(self):
         pris = list(filter(lambda x: x.get('primary'), self.columns))
